2c253931676df3324bdefda0cfd6653682ab5a96
CongSang <honguyencongsang723@gmail.com>
2022-08-23 14:46:00 +0700

    chat firebase

--- SocialMedia03.WEB/wwwroot/js/chatFireBase/firebaseConfig.js
+++ SocialMedia03.WEB/wwwroot/js/chatFireBase/firebaseConfig.js
@@ -1,13 +1,15 @@
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
+import "https://www.gstatic.com/firebasejs/8.10.0/firebase.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCx8_tP8idRcZAPI_g2qSKnM4YKS9wfZYg",
-    authDomain: "sharing-hope.firebaseapp.com",
-    projectId: "sharing-hope",
-    storageBucket: "sharing-hope.appspot.com",
-    messagingSenderId: "753475301856",
-    appId: "1:753475301856:web:2258022cf7014bfd9f3b85"
+    apiKey: "AIzaSyBrkgEivhiL2-0HdcU8aTSHwGpRohWQFgA",
+    authDomain: "socialmedia03-76bf8.firebaseapp.com",
+    databaseURL: "https://socialmedia03-76bf8-default-rtdb.firebaseio.com",
+    projectId: "socialmedia03-76bf8",
+    storageBucket: "socialmedia03-76bf8.appspot.com",
+    messagingSenderId: "703361284714",
+    appId: "1:703361284714:web:5d6e968a66296c91466813"
 };
 
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+export const db = firebase.database();
